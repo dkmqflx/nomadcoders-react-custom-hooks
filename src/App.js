2c4b5248acd9385ebf914a20db11bcf1862bca54
dkmqflx <dkmqflx@gmail.com>
2021-05-02 00:05:36 +0900

    #1.1 useInput

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import useInput from './useInput/useInput'
+
 
 function App() {
+  const name = useInput("")
   return (
     <div className="App">
-      hello react!
+      <h1>useInput</h1>
+      <input type="text" {...name}/>
+      {/* {...name}은 
+      value={name.value} onChange={name.onChnage}
+      를 대체한다 */}
     </div>
   );
 }
